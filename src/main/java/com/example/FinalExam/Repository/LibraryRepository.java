07b62fe5e4314e42b557200d656ba7f8733997e3
GOOSE <aiberiskendirov@gmail.com>
2025-12-18 02:58:39 +0500

    Add repositories

---
+++ src/main/java/com/example/FinalExam/Repository/LibraryRepository.java
@@ -0,0 +1,7 @@
+package com.example.FinalExam.Repository;
+
+import com.example.FinalExam.Model.Library;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibraryRepository extends JpaRepository<Library, Long> {
+}
